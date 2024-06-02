753920a215b8c0582e32b49b8ec0024f42c8de34
JoramirJr <joramirjunior@gmail.com>
2024-06-01 22:20:35 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
                         //exp: GET /banana HTTP/1.1\r\nHost: localhost:4221\r\n\r\n
                         let mut req_tokens = _request.split_whitespace();
                         let _ = req_tokens.next();
-                        let path = req_tokens.next().unwrap();
+                        let path = req_tokens.next();
                         print!("Path: {:?}", path);
 
                         // match path {
