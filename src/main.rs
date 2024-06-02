50373bc8a7770a6bd7db18dadec58afa1755cbf5
JoramirJr <joramirjunior@gmail.com>
2024-06-02 15:12:58 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,10 @@ fn main() {
             Ok(mut _stream) => {
                 println!("accepted new connection");
                 let mut request = String::new();
+                let _ = _stream.read_to_string(&mut request);
 
-                println!("STREAM: {:?}", _stream);
-                
+                println!("STREAM: {:?}", request);
+
                 let mut req_tokens = request.split_whitespace();
                 let _ = req_tokens.next();
                 let _path = req_tokens.next().unwrap();
@@ -26,7 +27,6 @@ fn main() {
                         let _ = _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n");
                     }
                 }
-                // let _ = _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
             }
             Err(e) => {
                 println!("error: {}", e);
