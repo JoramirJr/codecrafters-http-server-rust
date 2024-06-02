a876fab8c496ca7d6cc23389b81cff32d3d27abf
JoramirJr <joramirjunior@gmail.com>
2024-06-02 14:07:36 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::{io::{Read, Write}, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
-    
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
                 let mut buf = Vec::new();
-                let _ =_stream.read_to_end(&mut buf);
+                let _ = _stream.read_to_end(&mut buf);
                 let request = String::from_utf8(buf);
 
                 match request {
@@ -18,6 +20,9 @@ fn main() {
                         let mut req_tokens = _request.split_whitespace();
                         let first_seg = req_tokens.next();
                         println!("First Segment: {:?}", first_seg);
+                        let second_seg = req_tokens.next();
+                        println!("2nd Segment: {:?}", second_seg);
+
                         // let _path = req_tokens.next();
 
                         // match path {
@@ -26,7 +31,7 @@ fn main() {
                         //     }
                         //     _ => {
                         //         let _ = _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n");
-                        //     }   
+                        //     }
                         // }
                     }
                     Err(e) => {
