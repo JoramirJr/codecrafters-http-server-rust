a949bde6aa4daf7debb7ce836c638e9414d97438
JoramirJr <joramirjunior@gmail.com>
2024-06-02 15:08:44 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,22 @@ fn main() {
             Ok(mut _stream) => {
                 println!("accepted new connection");
                 let mut request = String::new();
-                let mut clone_stream = _stream.try_clone().unwrap();
-                let _ = clone_stream.read_to_string(&mut request);
 
-                // let mut req_tokens = request.split_whitespace();
-                // let _ = req_tokens.next();
-                // let _path = req_tokens.next().unwrap();
+                println!("STREAM: {:?}", _stream);
+                
+                let mut req_tokens = request.split_whitespace();
+                let _ = req_tokens.next();
+                let _path = req_tokens.next().unwrap();
 
-                // match _path {
-                //     "/" => {
-                //         let _ = _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
-                //     }
-                //     _ => {
-                //         let _ = _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n");
-                //     }
-                // }
-                let _ = _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
+                match _path {
+                    "/" => {
+                        let _ = _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
+                    }
+                    _ => {
+                        let _ = _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n");
+                    }
+                }
+                // let _ = _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
             }
             Err(e) => {
                 println!("error: {}", e);
