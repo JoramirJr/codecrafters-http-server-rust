74aa9ae1841260fd9507deb954e85a08eaa550ef
JoramirJr <joramirjunior@gmail.com>
2024-06-04 22:19:12 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::{
     net::TcpListener,
 };
 
-// fn extract_str_and_len(){
+fn extract_str_and_len(){
 
-// }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -24,7 +24,7 @@ fn main() {
                 match _path.chars().next().unwrap() {
                     '/' => {
                         let split_segs: Vec<&str> = _path.split("/").collect();
-                        println!("Path: {:?}", _path);
+                        println!("Req Tokens: {:?}", req_tokens);
                         //
                         let _ = _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
                     }
