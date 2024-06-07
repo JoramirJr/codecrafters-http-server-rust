5debaac516d346d37a02089d0fb382babaa84f07
JoramirJr <joramirjunior@gmail.com>
2024-06-07 15:32:01 -0300

    teste

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,11 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                 let stream_bytes = _stream
-                     .borrow()
-                     .bytes()
-                     .map(|bytes_result: Result<u8, std::io::Error>| bytes_result.unwrap());
-                //     .collect_vec();
+                let stream_bytes = _stream
+                    .borrow()
+                    .bytes()
+                    .map(|bytes_result: Result<u8, std::io::Error>| bytes_result.unwrap())
+                    .collect_vec();
                 // let request = String::from_utf8(stream_bytes).unwrap();
                 // let mut req_tokens = request.split_whitespace();
                 // let _ = req_tokens.next();
