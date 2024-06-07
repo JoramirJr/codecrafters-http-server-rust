22144c5dc25d7656f2b8f3afabe5e32765452410
JoramirJr <joramirjunior@gmail.com>
2024-06-07 14:52:03 -0300

    teste

--- src/main.rs
+++ src/main.rs
@@ -26,37 +26,37 @@ fn main() {
                 let _ = _stream.read(&mut buffer);
                 // let _ = _stream.read(&mut buffer);
                 let request = String::from_utf8(buffer.to_vec()).unwrap();
-                let mut req_tokens = request.split_whitespace();
-                let _ = req_tokens.next();
-                let _path = req_tokens.next().unwrap();
+                // let mut req_tokens = request.split_whitespace();
+                // let _ = req_tokens.next();
+                // let _path = req_tokens.next().unwrap();
 
                 println!("Request: {:?}", request);
 
-                match _path.chars().next().unwrap() {
-                    '/' => {
-                        let split_segs: Vec<&str> = _path.split("/").collect();
-                        let mut split_segs_noblank: Vec<&str> = Vec::new();
-                        for seg in split_segs.into_iter() {
-                            if seg != "" {
-                                split_segs_noblank.push(seg);
-                            }
-                        }
-                        if split_segs_noblank.len() == 1 {
-                            let _ = _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
-                        } else {
-                            let ExtractStrAndLenReturn {
-                                body,
-                                content_length,
-                            } = extract_str_and_len(split_segs_noblank);
-                            //HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc
-                            let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", content_length, body);
-                            let _ = _stream.write(response.as_bytes());
-                        }
-                    }
-                    _ => {
-                        let _ = _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n");
-                    }
-                }
+                // match _path.chars().next().unwrap() {
+                //     '/' => {
+                //         let split_segs: Vec<&str> = _path.split("/").collect();
+                //         let mut split_segs_noblank: Vec<&str> = Vec::new();
+                //         for seg in split_segs.into_iter() {
+                //             if seg != "" {
+                //                 split_segs_noblank.push(seg);
+                //             }
+                //         }
+                //         if split_segs_noblank.len() == 1 {
+                //             let _ = _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
+                //         } else {
+                //             let ExtractStrAndLenReturn {
+                //                 body,
+                //                 content_length,
+                //             } = extract_str_and_len(split_segs_noblank);
+                //             //HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc
+                //             let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", content_length, body);
+                //             let _ = _stream.write(response.as_bytes());
+                //         }
+                //     }
+                //     _ => {
+                //         let _ = _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n");
+                //     }
+                // }
                 let _ = _stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
             }
             Err(e) => {
