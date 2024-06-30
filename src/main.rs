e8aa999aa5844db4033425e108dcbf71e56b5560
JoramirJr <joramirjunior@gmail.com>
2024-06-29 22:28:50 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -42,9 +42,7 @@ fn main() {
                             if _path.starts_with("/files") {
                                 let path_arr: Vec<&str> = _path.split("/").collect_vec();
                                 let dir_file: Result<File, std::io::Error> =
-                                    File::open(path_arr[2]);
-
-                                println!("dir_file: {:?}", path_arr[2]);
+                                    File::open(format!("/{}", path_arr[2]));
 
                                 match dir_file {
                                     Ok(mut dir_file) => {
